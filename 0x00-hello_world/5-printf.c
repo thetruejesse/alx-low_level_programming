7815ae1403b472a660c5199b58bd4437e434b46a
thetruejesse <jesse4isooba@gmail.com>
2022-03-12 21:10:31 +0000

    corrections

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+/**
+ * Main - entry point to the code
+ *
+ * return 0, always a success
+ */
 
-#include <stdio.h>
-
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
