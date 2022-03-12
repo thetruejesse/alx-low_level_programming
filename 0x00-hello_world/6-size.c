df2a3de142cbf35984521715454fe9d95a43c6df
thetruejesse <jesse4isooba@gmail.com>
2022-03-12 20:25:24 +0000

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,6 +2,9 @@
 
 int main()
 {
-	printf("the size of an integer is: %ld",(sizeof(int)));
+	printf("the size of an integer is: %ld", sizeof(int));
+	printf("the size of long int: %d \n", sizeof(long int));
+	printf("the size of a char: %d \n", sizeof(char);
+	printf("the size of a float %d \n", sizeof(float));
 	return(0);
 }
