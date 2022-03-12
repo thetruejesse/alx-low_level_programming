3532dbf7b9f188af625d47b4f232e1740bc3c54c
thetruejesse <jesse4isooba@gmail.com>
2022-03-12 21:06:49 +0000

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,5 @@ printf("the size of long int: %d byte(s)\n", sizeof(long int));
 printf("the size of a char: %d byte(s)\n", sizeof(char));
 printf("the size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("the size of a float: %d byte(s)\n", sizeof(float));
-return(0);
+return (0);
 }
