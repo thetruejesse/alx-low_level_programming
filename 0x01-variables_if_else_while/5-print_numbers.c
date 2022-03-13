aff454d6e59e8063e00a344a669fcb7212c99943
thetruejesse <jesse4isooba@gmail.com>
2022-03-13 22:29:31 +0000

    Forth commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+
+int main(void)
+{
+int snum;
+
+for (snum = 0; snum < 10; snum++)
+{
+printf("%snum", snum);
+}
+}
