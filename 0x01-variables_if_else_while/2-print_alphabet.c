3f5f9cf2c1d33c5d276edf793f353cda1fb07741
thetruejesse <jesse4isooba@gmail.com>
2022-03-13 21:38:31 +0000

    My 2nd commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success/correct)
+ */
+
+int main(void)
+{
+char alpha;
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+putchar(alpha);
+}
+
+putchar('\n');
+
+return(0);
+}
