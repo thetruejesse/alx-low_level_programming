8aa900c0f80de3d35fa78fc88e1b3501807c5542
thetruejesse <jesse4isooba@gmail.com>
2022-03-13 23:16:48 +0000

    My commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return; Always 0
+ */
+
+int main(void)
+{
+int num;
+
+for (num = 48; num <= 57; num++)
+putchar(num)
+{
+if(num == 57)
+break;
+}
+putchar('\n');
+putchar(' ');
+
+
+return (0);
+}
