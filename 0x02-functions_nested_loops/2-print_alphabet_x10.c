ded002315abe8ea03cd9ae5c0effd9fbd09e3c81
thetruejesse <jesse4isooba@gmail.com>
2022-03-16 00:49:11 +0000

    my commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+int alpha;
+int count;
+
+count = 0;
+while (count < 10)
+{
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+_putchar(alpha);
+}
+
+count++;
+_putchar('\n');
+}
+}
