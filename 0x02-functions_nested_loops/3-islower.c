fb3149e97dd44270e7c2de82a6ad0bbe61d830f8
thetruejesse <jesse4isooba@gmail.com>
2022-03-16 00:57:42 +0000

    my commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - fjdklsa
+ *
+ * return 0: code success
+ */
+
+int _islower(int c)
+{
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+
+}
