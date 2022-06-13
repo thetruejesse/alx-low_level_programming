ef73dbe181093717547f3b8150f7bddddd7e3dba
thetruejesse <jesse4isooba@gmail.com>
2022-06-13 18:20:17 +0000

    Corrections

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-#indef MAIN_H
-=======
 #ifndef MAIN_H
->>>>>>> 347dd166ba93ea38746a3496115b54db0081630e
 #define MAIN_H
 int _putchar(char c);
 int _islower(int c);
@@ -11,29 +7,17 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-<<<<<<< HEAD
-int _puts(char *s);
-=======
 void _puts(char *s);
->>>>>>> 347dd166ba93ea38746a3496115b54db0081630e
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-<<<<<<< HEAD
-int *strcmp(char *s1, char *s2);
-=======
 int _strcmp(char *s1, char *s2);
->>>>>>> 347dd166ba93ea38746a3496115b54db0081630e
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-<<<<<<< HEAD
-char *strstr(char *haystack, char *needle);
-=======
 char *_strstr(char *haystack, char *needle);
->>>>>>> 347dd166ba93ea38746a3496115b54db0081630e
 #endif
