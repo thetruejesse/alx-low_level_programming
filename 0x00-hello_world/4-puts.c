7b8d565485bc8f28134b7ba47e54b5c404a49186
Jesse Isooba <jesse4isooba@gmail.com>
2023-03-16 21:28:55 +0300

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * program that prints string to stdio
+ *
+ * return: 0
+ */
+
+int main()
+{
+	printf("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
