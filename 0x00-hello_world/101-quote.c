072413e1dcef97483534a64464cdab981889bc2c
Jesse Isooba <jesse4isooba@gmail.com>
2023-03-17 03:30:04 +0300

    Quota

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point of the program
  *
- * return 1.
+ * return 1 (failure)
  **/
 int main()
 {
-
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
