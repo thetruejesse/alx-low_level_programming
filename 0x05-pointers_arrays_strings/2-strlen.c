bbdd1f49f86ac5bac900ca2c4b2c7db7b562997a
Jesse Isooba <jesse4isooba@gmail.com>
2023-03-27 17:25:19 +0300

    Length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen - function that counts number of chars in a string
+ * @s: pointer to first char in string
+ *
+ * Return: nothing.
+ */
+int _strlen(char *s[])
+{
+	int s;
+	for (s = 0, s != '\0', s++)
+	{
+	return s;
+	}
+}
