b0f0cffc7d1987e9b6361c232c8f6dba28aa783d
Jesse Isooba <jesse4isooba@gmail.com>
2023-03-27 18:10:55 +0300

    Put string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,5 +6,5 @@
  */
 void _puts(char *str)
 {
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	*str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
 }
