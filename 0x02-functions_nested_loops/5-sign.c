b31e299f02b0395da582211fa0f878faed9aca0d
Jesse Isooba <jesse4isooba@gmail.com>
2023-04-24 16:31:15 +0300

    Signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,14 +12,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1, putchar('+'));
+		return (putchar('+') 1);
 	}
 	else if (n == 0)
 	{
-		return (0, putchar('0'));
+		return (putchar('0') 0);
 	}
 	else
 	{
-		return (-1, putchar('-'));
+		return (putchar('-') 1);
 	}
 }
