033891a950b08bc08076837feab8393480c1e0a5
Jesse Isooba <jesse4isooba@gmail.com>
2023-04-24 12:37:04 +0300

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar('0' + i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
