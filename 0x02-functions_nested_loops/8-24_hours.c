25ed35e728d5fa3618e77b5305536cb844c7b91c
Jesse Isooba <jesse4isooba@gmail.com>
2023-05-01 12:19:34 +0300

    There goes the time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * jack_bauer - Prints every minute of the day of 
- * 		Jack bauer, starting from 00:00 to 23:59.
+ * jack_bauer - method that prints time
+ *
  */
+
 void jack_bauer(void)
 {
 	int hour, minute;
