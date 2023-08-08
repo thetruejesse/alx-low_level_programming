865b069941e8003f82c71febc7889ce004485b0a
Jesse <jesse4isooba@gmail.com>
2023-08-08 04:47:38 +0300

    Print all arguments that you recieve

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments it recieves
+ *
+ * @argc - number of arguments
+ * @argv - string of arguments passed
+ *
+ * Return: 0 - Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	if (argc > 1)
+	{
+		for (count = 0; count < argc; count++)
+		{
+			printf("%s\n", argv[count]);
+		}
+	}
+	else
+	{
+		printf("Dont count\n");
+	}
+
+	return (0);
+}
